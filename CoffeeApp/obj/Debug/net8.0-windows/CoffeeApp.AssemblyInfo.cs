f89f1068bca0f62bd79e1567962c978204caf018
illyushanskii <illya.storozhenko21@gmail.com>
2024-09-30 21:50:11 +0300

    sanyaa

--- CoffeeApp/obj/Debug/net8.0-windows/CoffeeApp.AssemblyInfo.cs
+++ CoffeeApp/obj/Debug/net8.0-windows/CoffeeApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a84b86bb87e6eb92d870d9bc006db29554633af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fd1cc7cdff319baaeef556ce108b8eeb44446fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
